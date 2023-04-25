e733d50fdcccdf6676669814e0c45b4a8aeb16dc
madprofessor <madprofessor42@yandex.ru>
2023-04-25 18:00:37 +0300

    ..

--- js_code/2.1 - Типы данных/6 - Список(Объект)/Задачи/findMatches.js
+++ js_code/2.1 - Типы данных/6 - Список(Объект)/Задачи/findMatches.js
@@ -8,3 +8,47 @@ function scramble(str1, str2) {
 
 console.log(scramble('cedewaraaossoqqyt', 'codewars'))
 
+/**
+ * Найти все книги Ремарка на полках
+ */
+const findRemark = (shelves, books) => {
+    const filteredBooks = (
+        Object.keys(books)
+            .filter((key) => ['ремарк'].includes(books[key]))
+    );
+
+    const filteredShelves = (
+        Object.keys(shelves)
+            .filter(key => shelves[key]
+                .some(el => filteredBooks.includes(el)))
+    );
+
+    return filteredShelves
+}
+
+
+const shelves = {
+    "полка 1": ["1984", "три товарища"],
+    "полка 2": ["убить пересмешника", "герой нашего времени"],
+    "полка 3": ["10 негритят", "на западном фронте без перемен"]
+};
+const books = {
+    "1984": "оруэлл",
+    "три товарища": "ремарк",
+    "убить пересмешника": "ли",
+    "герой нашего времени": "лермонтов",
+    "10 негритят": "кристи",
+    "на западном фронте без перемен": "ремарк"
+};
+
+
+console.log(findRemark(shelves, books))
+
+
+
+
+
+
+
+
+
